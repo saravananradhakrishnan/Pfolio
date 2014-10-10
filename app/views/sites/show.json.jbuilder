e5b1cee9d53a1fedfb70fc07070bc06a58130163
mirzajuned <mirzajuned42@gmail.com>
2014-10-10 20:22:10 +0530

    Theme and Site

---
+++ app/views/sites/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @site, :id, :name, :user_id, :category_id, :created_at, :updated_at
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141010115300) do
+ActiveRecord::Schema.define(version: 20141010140455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "sites", force: true do |t|
+    t.string   "name"
+    t.integer  "user_id"
+    t.integer  "category_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "themes", force: true do |t|
+    t.string   "name"
+    t.string   "description"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
